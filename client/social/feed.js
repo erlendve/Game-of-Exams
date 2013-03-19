b513b2095c2c76d5541ec57c65ae0175253e921a
Erlend Vestad <erlendve@hotmail.com>
2013-03-19 11:57:03 +0100

    added zombie to under construction and set max-height for feed boxes

--- client/social/feed.js
+++ client/social/feed.js
@@ -1,9 +1,7 @@
 Template.feed.live = function() {
-  //sort reverse hack
   //return Answers.find({saved: true}, {sort: {$natural:-1}}).fetch().reverse();
-  //return Answers.find({saved: true}).fetch().reverse();
-  return Answers.find({exercise_id: "b3ee729a-dbd2-4a38-a629-0c0410de9d50"}).fetch().reverse();
-}
+  return Answers.find({exercise_id: "b3ee729a-dbd2-4a38-a629-0c0410de9d50"}, {sort: {createdAt: 1}, limit: 20});
+};
 
 Template.feed.rendered = function() {
   $('code').each(function(i, e) {hljs.highlightBlock(e)});
